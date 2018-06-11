c6b173cf39fea1b6cf9def8eaba0aa30c07a3e35
mithunrajarackal <mithun.villae@gmail.com>
2018-06-11 10:09:22 +0530

    added arithmetic exception error

--- src/test/scala/org/me/General/AssertionTest.scala
+++ src/test/scala/org/me/General/AssertionTest.scala
@@ -7,15 +7,15 @@ class AssertionTest extends FlatSpec {
   // normal assertions
   val left = 2
   val right = 1
-  assert(left == right)  // assertion returning true
+  assert(left != right)  // assertion returning true
 
-  assert(left !=  right)  // assertion returning false
+//  assert(left ==  right)  // assertion returning false
 
 
   //  when operands are lengthy expressions
   val a = 5
   val b = 2
-  assertResult(2) {
+  assertResult(3) {
     a - b
   }
 
